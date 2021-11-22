e33dfcac45ab702470b72811c34ede0892dc8548
alex whitehouse <alex.whitehouse8@gmail.com>
2021-11-22 23:36:42 +0000

    User Logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/trails", to: "trails#index"
   get "/trails/:trail_id", to: "trails#show", as: "trail"
