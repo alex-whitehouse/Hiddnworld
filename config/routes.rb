de1538b0e8c8cbb8dfc4b2b751e246c4389402b1
alex whitehouse <alex.whitehouse8@gmail.com>
2021-11-23 01:28:19 +0000

    Log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   resources :users
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/trails", to: "trails#index"
   get "/trails/:trail_id", to: "trails#show", as: "trail"
+  get "sessions/", to: "trails#index"
   root "trails#index"
 end
