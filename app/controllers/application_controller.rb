6965ae8c3411918e0515bb640be31d6f240ee0b4
alex whitehouse <alex.whitehouse8@gmail.com>
2021-11-23 23:19:09 +0000

    Logout working, need to add auth method to restricted actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ private
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def authorise
+    redirect_to login_url, alert: "Not Authorised" if current_user.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+get "signup", to: "users#new", as: "signup"
+get "login", to: "sessions#new", as: "login"
+get "logout", to: "sessions#destroy", as: "logout"
 
   resources :users
+  get "/sessions/current", to: "sessions#destroy"
+  
   resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/trails", to: "trails#index"
   get "/trails/:trail_id", to: "trails#show", as: "trail"
-  get "/sessions/current", to: "sessions#destroy"
+  
   root "trails#index"
 end
