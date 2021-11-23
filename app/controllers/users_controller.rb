d2ee023952fad4b7c1efd441bae6f37c75967dd5
alex whitehouse <alex.whitehouse8@gmail.com>
2021-11-23 00:12:39 +0000

    Login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ end
 def create
   @user = User.new(params[:user])
   if @user.save
-    session[:user_id] = @user.id
-    redirect_to root_url, notice: "Thank you for signing up!"
-  else
-    render "new"
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Thank you for signing up!"
+    else
+      render "new"
+    end
   end
 end
