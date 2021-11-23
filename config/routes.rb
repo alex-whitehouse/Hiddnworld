4f7970f47e2fd3440a0baba3dee93b0253913652
alex whitehouse <alex.whitehouse8@gmail.com>
2021-11-23 22:32:14 +0000

    log out redirect issue

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/trails", to: "trails#index"
   get "/trails/:trail_id", to: "trails#show", as: "trail"
-  get "sessions/", to: "trails#index"
+  get "/sessions/current", to: "sessions#destroy"
   root "trails#index"
 end
