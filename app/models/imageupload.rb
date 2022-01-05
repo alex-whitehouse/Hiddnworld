0fd434893f84485f2f7a80d4a81e63dfe0311c52
Alex Whitehouse <alex.whitehouse8@gmail.com>
2022-01-05 13:57:17 +0000

    delete image upload

--- app/models/imageupload.rb
+++
@@ -1,5 +0,0 @@
-class Imageupload < ApplicationRecord
-    *app/model/ApplicationRecord.rb*
- 
-mount_uploader :image, ImageUploader
-end
--- test/models/imageupload_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ImageuploadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
