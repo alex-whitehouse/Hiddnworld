5c1f1957b56f721d0b5bb38e5fd6c98d226201b1
Alex Whitehouse <alex.whitehouse8@gmail.com>
2022-01-06 01:56:41 +0000

    CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,14 @@ module ApplicationHelper
   
   
   def flash_class(level)
-    case level
-        when :notice then "alert alert-info"
-        when :success then "alert alert-success"
-        when :error then "alert alert-error"
-        when :alert then "alert alert-error"
-    end
+    bootstrap_alert_class = {
+      "success" => "alert-success",
+      "error" => "alert-danger",
+      "notice" => "alert-info",
+      "alert" => "alert-danger",
+      "warn" => "alert-warning"
+    }
+    bootstrap_alert_class[level]
+  end
+  
 end
